940c7e19413d75bed1fe14bf97009c6c77f773f0
Nova <145818807+codern0va@users.noreply.github.com>
2024-04-10 13:02:52 -0400

    Update stage1.hx

--- data/stages/stage1.hx
+++ data/stages/stage1.hx
@@ -2,6 +2,11 @@ var offX:Float = -1000/2;
 var offY:Float = -500/2;
 
 function create(){
+    var cave = new FlxSprite(offX + 711, offY + 25);
+	cave.loadGraphic(Paths.image("stages/stage1/cave"));	
+	cave.scrollFactor.set(0.8, 0.75);
+    insert(members.indexOf(gf), cave);
+
     var graphic = Paths.image("stages/stage1/ground");
 	
 	var ground = new FlxSprite(offX, offY + 710);
@@ -32,5 +37,6 @@ function create(){
 	add(groundFiller);
 	add(lground);
 	add(rground);
+    add(cave);
 	add(ground);
 }
